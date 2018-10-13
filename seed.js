cea8ce7a600b209a7b547aac2730457fa011a325
Tiffany J Brown <tiffany.brownjc@gmail.com>
2018-10-13 17:00:22 -0400

    seed file done

--- seed.js
+++ seed.js
@@ -31,26 +31,26 @@ const main = async () => {
     address: "17 William Street, New York, New York 10004",
     allergyFriendly: "Gluten-Free Options, Peanut-Free Options" 
   });
-const cochinita = await Food_Establishment.create({
-  name: "Cochinita Tacos:",
-  address: "922 Fulton Street, Brooklyn, NY 11238",
-  allergyFriendly: "Dairy-Free Options, Gluten-Free Options, Nut-Free Kitchen" 
-});
-const byChloe = await Food_Establishment.create({
-  name: "By Chloe",
-  address: "185 Bleecker St, New York, NY 10012",
-  allergyFriendly: "Dairy-Free Kitchen, Gluten-Free Options" 
-});
-const colors = await Food_Establishment.create({
-  name: "Colors",
-  address: "417 Lafayette Street, New York, New York 10003",
-  allergyFriendly: "Gluten-Free Kitchen" 
-});
-const huKitchen = await Food_Establishment.create({
-  name: "Hu Kitchen",
-  address: "78 5th Avenue, New York, New York 10011",
-  allergyFriendly: "Dairy-Free Kitchen, Gluten-Free Kitchen" 
-});
+  const cochinita = await Food_Establishment.create({
+    name: "Cochinita Tacos:",
+    address: "922 Fulton Street, Brooklyn, NY 11238",
+    allergyFriendly: "Dairy-Free Options, Gluten-Free Options, Nut-Free Kitchen" 
+  });
+  const byChloe = await Food_Establishment.create({
+    name: "By Chloe",
+    address: "185 Bleecker St, New York, NY 10012",
+    allergyFriendly: "Dairy-Free Kitchen, Gluten-Free Options" 
+  });
+  const colors = await Food_Establishment.create({
+    name: "Colors",
+    address: "417 Lafayette Street, New York, New York 10003",
+    allergyFriendly: "Gluten-Free Kitchen" 
+  });
+  const huKitchen = await Food_Establishment.create({
+    name: "Hu Kitchen",
+    address: "78 5th Avenue, New York, New York 10011",
+    allergyFriendly: "Dairy-Free Kitchen, Gluten-Free Kitchen" 
+  });
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
   const dairy = await Allergy.create({
     type: "Dairy"
@@ -83,7 +83,40 @@ const huKitchen = await Food_Establishment.create({
     type: "Sulfite"   
   });
 
-  
+  await bigDaddy.addAllergy(dairy);
+  await bigDaddy.addAllergy(gluten);
+  await bigDaddy.addAllergy(treeNut);
+
+  await bigDaddyTwo.addAllergy(dairy);
+  await bigDaddyTwo.addAllergy(gluten); 
+  await bigDaddyTwo.addAllergy(treeNut);
+
+  await bistango.addAllergy(dairy);
+  await bistango.addAllergy(gluten);
+  await bistango.addAllergy(treeNut);
+
+  await bistangoTwo.addAllergy(dairy);
+  await bistangoTwo.addAllergy(gluten);
+  await bistangoTwo.addAllergy(treeNut);
+
+  await blueSmoke.addAllergy(dairy);
+  await blueSmoke.addAllergy(gluten);
+  await blueSmoke.addAllergy(treeNut);
+
+  await crepes.addAllergy(gluten);
+  await crepes.addAllergy(peanut);
+
+  await cochinita.addAllergy(dairy);
+  await cochinita.addAllergy(gluten);
+  await cochinita.addAllergy(treeNut);
+
+  await byChloe.addAllergy(dairy);
+  await byChloe.addAllergy(gluten);
+
+  await colors.addAllergy(gluten);
+
+  await huKitchen.addAllergy(dairy);
+  await huKitchen.addAllergy(gluten);
 
   process.exit();
 }
