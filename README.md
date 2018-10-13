70ba662048d7d4f20dea7795695c609e74f0fca8
Tiffany J Brown <tiffany.brownjc@gmail.com>
2018-10-13 14:31:15 -0400

    models

--- README.md
+++ README.md
@@ -1,31 +1,4 @@
-# Full stack app boilerplate
+Safe to Eat
 
-This is boilerplate for a full-stack app.
-
-## Install JS dependencies
-
-```
-npm install
-```
-
-## Modify the database name
-
-The database name is defined in `createDb.sql` and `models.js`. You'll need to edit them by hand for now.
-
-## Create the database
-
-```
-npm run db
-```
-
-This will drop the database, recreate it, create tables defined in `models.js`, and seed the database with instance data from `seed.js`
-
-## Run the development server
-
-This will start both the Express API server and the React application.
-
-```
-npm run dev
-```
-A food allergy application, that is based on location. When traveling able to find restaurants that are food allergy friendly. Users can put in their choice of foods to stay away from and the app saves that info based on the user login and when traveling recommends restaurants, deli, bakeries, cafes that is safe to eat at.
+A food allergy application based on location. Able to find restaurants/eateries that are food allergy friendly. Users can put in their choice of foods to stay away from and the app saves that info based on the user login and when they need a recommendation on where to eat they get restaurants, deli, bakeries, cafes that is safe to eat at in their location.
 
--- models.js
+++ models.js
@@ -4,12 +4,62 @@ const sequelize = new Sequelize({
   database: 'some_app_name',
   dialect: 'postgres'
 });
-
-
 // Create models here
+const User = sequelize.define('user', {
+  username: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.TEXT,
+  },
+  favorites: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER)
+  }
+});
+
+
+
+const Food_Establishment = sequelize.define('foodPlace', {
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  address: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  allergyFriendly: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+});
+
+const Allergy = sequelize.define('allergy', {
+  TYPE: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+});
+
+const UserAllergy = sequelize.define('userAllergy');
+const FoodEstAllergy = sequelize.define('foodEstAllergy');
+
+User.belongsToMany(Allergy, { through: UserAllergy });
+Allergy.belongsToMany(User, { through: UserAllergy });
+
+Food_Establishment.belongsToMany(Allergy, { through: FoodEstAllergy });
+Allergy.belongsToMany(Food_Establishment, { through: FoodEstAllergy });
 
 
 module.exports = {
   // Export models
+  User, 
+  Food_Establishment, 
+  Allergy,
   sequelize: sequelize
 };
